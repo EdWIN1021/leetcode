0e3429d03cfdfa8f2447cce8fbf0380694e12f5e
Edwin <>
2023-10-02 19:29:45 -0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var lengthOfLastWord = function (s) {
-  const str = s.trim().split(" ");
-  return str[str.length - 1].length;
+var reverseWords = function (s) {
+  let result = "";
+  let right = s.length;
+  let left = right - 1;
+
+  while (left >= -1) {
+    if ((s[left] === " " || !s[left]) && s[left - 1] !== " ") {
+      result += s.slice(left + 1, right).trim() + " ";
+      right = left;
+    }
+    left--;
+  }
+  return result.trim();
 };
 
-console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(reverseWords("the sky is  blue"));
