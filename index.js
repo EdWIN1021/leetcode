9173d4700a4be77d1e826d63ce00284f27ac38ae
Edwin <>
2023-10-26 16:01:14 -0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-var minSubArrayLen = function (target, nums) {
-  let result = Infinity;
+var spiralOrder = function (matrix) {
   let left = 0;
-  let right = 0;
-  let total = 0;
+  let right = matrix[0].length;
+  let top = 0;
+  let bottom = matrix.length;
 
-  while (right < nums.length) {
-    total += nums[right];
-    if (total < target) {
-      right++;
-    } else {
-      total = total - nums[left] - nums[right];
-      result = Math.min(result, right - left + 1);
-      left++;
+  let result = [];
+
+  while (left < right && top < bottom) {
+    for (let i = left; i < right; i++) {
+      result.push(matrix[left][i]);
     }
+
+    top++;
+
+    for (let i = top; i < bottom; i++) {
+      result.push(matrix[i][right - 1]);
+    }
+
+    right--;
+
+    if (!(left < right && top < bottom)) {
+      break;
+    }
+
+    for (let i = right - 1; i >= left; i--) {
+      result.push(matrix[bottom - 1][i]);
+    }
+
+    bottom--;
+
+    for (let i = bottom - 1; i >= top; i--) {
+      result.push(matrix[i][left]);
+    }
+
+    left++;
   }
 
-  return result === Infinity ? 0 : result;
+  return result;
 };
 
-console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
+console.log(spiralOrder([[7], [9], [6]]));
