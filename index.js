4ef551330f5e586cbb9ab009fe890e645334224e
Edwin <>
2023-11-09 09:47:15 -0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-var canConstruct = function (ransomNote, magazine) {
-  const map = new Map();
-
-  for (let i = 0; i < magazine.length; i++) {
-    if (!map.has(magazine[i])) {
-      map.set(magazine[i], 1);
-    } else {
-      map.set(magazine[i], map.get(magazine[i]) + 1);
-    }
-  }
-
-  for (let i = 0; i < ransomNote.length; i++) {
-    if (!map.has(ransomNote[i]) || map.get(ransomNote[i]) === 0) {
-      return false;
-    } else {
-      map.set(ransomNote[i], map.get(ransomNote[i]) - 1);
-    }
-  }
-  return true;
+var maxDepth = function (root) {
+  if (!root) return 0;
+  let left = maxDepth(root.left);
+  let right = maxDepth(root.right);
+  return Math.max(left, right) + 1;
 };
-
-console.log(canConstruct("aaa", "aaab"));
