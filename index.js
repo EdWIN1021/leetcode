fa9f4945230b2011f74691cc2938b92a12d845cb
Edwin <>
2023-11-10 09:46:11 -0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-var maxDepth = function (root) {
-  if (!root) return 0;
-  let left = maxDepth(root.left);
-  let right = maxDepth(root.right);
-  return Math.max(left, right) + 1;
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  const mapT = new Map();
+  const mapS = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    console.log(i);
+    if (!mapT.get(s[i])) {
+      mapT.set(s[i], t[i]);
+    }
+
+    if (!mapS.get(t[i])) {
+      mapS.set(t[i], s[i]);
+    }
+
+    if (
+      (mapT.get(s[i]) && mapT.get(s[i]) !== t[i]) ||
+      (mapS.get(t[i]) && mapS.get(t[i]) !== s[i])
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 };
+
+console.log(isIsomorphic("eea", "add"));
