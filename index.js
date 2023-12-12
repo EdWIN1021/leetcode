c222ff6969a8b1f5394fc957a03263da4839af6c
Edwin <>
2023-12-12 12:56:01 -0800

    feat: update

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-var isAnagram = function (s, t) {
+var containsNearbyDuplicate = function (nums, k) {
   const map = new Map();
-  if (s.length !== t.length) return false;
 
-  for (let i = 0; i < s.length; i++) {
-    if (!map.get(s[i])) {
-      map.set(s[i], 1);
+  for (let i = 0; i < nums.length; i++) {
+    if (!map.has(nums[i])) {
+      map.set(nums[i], i);
+    } else if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+      return true;
     } else {
-      map.set(s[i], map.get(s[i]) + 1);
+      map.set(nums[i], i);
     }
   }
 
-  for (let i = 0; i < t.length; i++) {
-    if (map.get(t[i])) {
-      map.set(t[i], map.get(t[i]) - 1);
-    } else {
-      return false;
-    }
-    map.get(t[i]) === 0 && map.delete(t[i]);
-  }
-
-  return map.size === 0;
+  return false;
 };
 
-console.log(isAnagram("anagram", "nagaram"));
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1));
