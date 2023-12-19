d799f077c8b8795fc12d38cbf3621ab94fa746cc
Edwin <>
2023-12-19 09:37:49 -0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var hasPathSum = function (root, targetSum) {
-  if (!root) return false;
-
-  if (!root.left && !root.right) {
-    return targetSum === root.val;
-  } else {
-    return (
-      hasPathSum(root.left, targetSum - root.val) ||
-      hasPathSum(root.right, targetSum - root.val)
-    );
+var removeElement = function (nums, val) {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
   }
 };
+
+removeElement([3, 2, 2, 3], 3);
