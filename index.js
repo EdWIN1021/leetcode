1030c9c02ac2d951fe39f92357de1d42d04ede23
Edwin <>
2023-12-20 09:54:50 -0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var removeDuplicates = function (nums) {
-  let i = 0;
-  while (i < nums.length) {
-    if (nums[i] === nums[i + 1]) {
-      nums.splice(i, 1);
-    } else {
-      i++;
-    }
-  }
+var countNodes = function (root) {
+  if (!root) return 0;
+  return countNodes(root.left) + countNodes(root.right) + 1;
 };
-
-removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
