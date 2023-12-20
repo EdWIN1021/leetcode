170fd7a2fd7a31fb497b8e48134849c6728a29f2
Edwin <>
2023-12-20 10:30:17 -0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-var countNodes = function (root) {
-  if (!root) return 0;
-  return countNodes(root.left) + countNodes(root.right) + 1;
+var removeDuplicates = function (nums) {
+  let i = 0;
+
+  let counter = 1;
+
+  while (i < nums.length) {
+    if (nums[i] === nums[i + 1] && counter < 2) {
+      counter++;
+      i++;
+    } else if (nums[i] === nums[i + 1] && counter === 2) {
+      nums.splice(i, 1);
+    } else if (nums[i] !== nums[i + 1]) {
+      counter = 1;
+      i++;
+    }
+  }
+
+  console.log(nums);
 };
+
+removeDuplicates([1, 2, 2, 2]);
