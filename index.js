4289b831a48ed629e2d946f7e26172aa3d371fc1
Edwin <edwinshi1021@hotmail.com>
2023-12-28 11:25:38 -0800

    update

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-var addBinary = function (a, b) {
-  let result = [];
-  a = a.split("").reverse();
-  b = b.split("").reverse();
-
-  let carry = 0;
-
-  for (let i = 0; i < Math.max(a.length, b.length); i++) {
-    let sum = Number(a[i] || 0) + Number(b[i] || 0);
-
-    if (sum > 1) {
-      result.unshift(carry);
-      carry = 1;
-    } else {
-      result.unshift(sum + carry > 1 ? 0 : sum + carry);
-      carry = sum + carry > 1 ? 1 : 0;
-    }
+function reverseBits(n) {
+  let result = 0;
+  for (let i = 0; i < 32; i++) {
+    result = (result << 1) | (n & 1);
+    n >>>= 1; // Use unsigned right shift to fill with zeros
   }
+  return result >>> 0; // Ensure the result is treated as an unsigned integer
+}
 
-  if (carry === 1) result.unshift(1);
-  return result.join("");
-};
-
-console.log(addBinary("1111", "1111"));
+let n = 43261596;
+let result = reverseBits(n);
+console.log(result);
