cdcbe3ca4038506f755648792c5dc22dbc9cd3c1
Edwin <edwinshi1021@hotmail.com>
2024-01-02 21:34:03 -0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-var canJump = function (nums) {
-  let right = nums.length - 1;
-  let left = right - 1;
+var isValidSudoku = function (board) {
+  let rows = new Map();
+  let cols = new Map();
+  let boxes = new Map();
 
-  while (left >= 0) {
-    if (nums[left] >= right - left) {
-      right = left;
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] === ".") {
+        continue;
+      }
+
+      if (rows.get(i) && rows.get(i).includes(board[i][j])) {
+        return false;
+      }
+
+      if (cols.get(j) && cols.get(j).includes(board[i][j])) {
+        return false;
+      }
+
+      if (
+        boxes.get(`${Math.floor(i / 3)},${Math.floor(j / 3)}`) &&
+        boxes
+          .get(`${Math.floor(i / 3)},${Math.floor(j / 3)}`)
+          .includes(board[i][j])
+      ) {
+        return false;
+      }
+
+      if (!rows.has(i)) {
+        rows.set(i, [board[i][j]]);
+      } else {
+        rows.set(i, [...rows.get(i), board[i][j]]);
+      }
+
+      if (!cols.has(j)) {
+        cols.set(j, [board[i][j]]);
+      } else {
+        cols.set(j, [...cols.get(j), board[i][j]]);
+      }
+
+      if (!boxes.has(`${Math.floor(i / 3)},${Math.floor(j / 3)}`)) {
+        boxes.set(`${Math.floor(i / 3)},${Math.floor(j / 3)}`, [board[i][j]]);
+      } else {
+        boxes.set(`${Math.floor(i / 3)},${Math.floor(j / 3)}`, [
+          ...boxes.get(`${Math.floor(i / 3)},${Math.floor(j / 3)}`),
+          board[i][j],
+        ]);
+      }
     }
-    left--;
   }
 
-  return right === 0;
+  return true;
 };
 
-console.log(canJump([2, 3, 1, 1, 4]));
+console.log(
+  isValidSudoku([
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", "6", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+
+    [".", ".", ".", ".", "8", ".", ".", ".", "."],
+    ["9", ".", ".", ".", "7", "5", ".", ".", "."],
+    [".", ".", ".", ".", "5", ".", ".", "8", "."],
+
+    [".", ".", "9", ".", ".", ".", ".", ".", "."],
+    ["2", ".", "6", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+  ])
+);
