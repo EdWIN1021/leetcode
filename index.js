58510d532c8dd94773a30ee49248eb487f9f406f
Edwin <edwinshi1021@hotmail.com>
2024-01-05 11:52:46 -0800

    update

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-var setZeroes = function (matrix) {
-  let rowZero = 1;
+var productExceptSelf = function (nums) {
+  const prefix = [1];
+  let postfix = 1;
 
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      if (matrix[i][j] === 0) {
-        matrix[0][j] = 0;
-
-        if (i > 0) {
-          matrix[i][0] = 0;
-        } else {
-          rowZero = 0;
-        }
-      }
-    }
+  for (let i = 1; i < nums.length; i++) {
+    prefix[i] = nums[i - 1] * prefix[i - 1];
   }
 
-  for (let i = 1; i < matrix.length; i++) {
-    for (let j = 1; j < matrix[0].length; j++) {
-      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
-        matrix[i][j] = 0;
-      }
-    }
+  for (let i = nums.length - 1; i >= 0; i--) {
+    prefix[i] *= postfix;
+    postfix *= nums[i];
   }
 
-  if (matrix[0][0] === 0) {
-    for (let i = 0; i < matrix.length; i++) {
-      matrix[i][0] = 0;
-    }
-  }
-
-  if (rowZero === 0) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      matrix[0][j] = 0;
-    }
-  }
+  return prefix;
 };
 
-setZeroes([
-  [0, 1, 2, 0],
-  [3, 4, 5, 2],
-  [1, 3, 1, 5],
-]);
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
