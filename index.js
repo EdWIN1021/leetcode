8df14407e4dc9032d2d8adc062161a5e82656e8d
Edwin <edwinshi1021@hotmail.com>
2024-01-05 09:33:24 -0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-var hIndex = function (citations) {
-  arr = citations.sort((a, b) => a - b);
-  let count = 0;
+var setZeroes = function (matrix) {
+  let rowZero = 1;
 
-  for (let i = citations.length - 1; i >= 0; i--) {
-    if (citations[i] > count) {
-      count++;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[0][j] = 0;
+
+        if (i > 0) {
+          matrix[i][0] = 0;
+        } else {
+          rowZero = 0;
+        }
+      }
     }
   }
-  return count;
+
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (matrix[0][0] === 0) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  if (rowZero === 0) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix[0][j] = 0;
+    }
+  }
 };
 
-console.log(hIndex([3, 0, 6, 1, 5]));
+setZeroes([
+  [0, 1, 2, 0],
+  [3, 4, 5, 2],
+  [1, 3, 1, 5],
+]);
