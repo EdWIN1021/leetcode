3d8bf1858d367d16e6204835eda203fb680bcee3
Edwin <edwinshi1021@hotmail.com>
2024-01-04 19:52:37 -0800

    update

--- index.js
+++ index.js
@@ -1,65 +1,16 @@
-var isValidSudoku = function (board) {
-  let rows = new Map();
-  let cols = new Map();
-  let boxes = new Map();
+var jump = function (nums) {
+  let max = 0;
+  let currMax = 0;
+  let result = 0;
 
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      let currBoxIndex = `${Math.floor(i / 3)},${Math.floor(j / 3)}`;
-      if (board[i][j] === ".") {
-        continue;
-      }
-
-      if (rows.get(i) && rows.get(i).includes(board[i][j])) {
-        return false;
-      }
-
-      if (cols.get(j) && cols.get(j).includes(board[i][j])) {
-        return false;
-      }
-
-      if (
-        boxes.get(currBoxIndex) &&
-        boxes.get(currBoxIndex).includes(board[i][j])
-      ) {
-        return false;
-      }
-
-      if (!rows.has(i)) {
-        rows.set(i, [board[i][j]]);
-      } else {
-        rows.set(i, [...rows.get(i), board[i][j]]);
-      }
-
-      if (!cols.has(j)) {
-        cols.set(j, [board[i][j]]);
-      } else {
-        cols.set(j, [...cols.get(j), board[i][j]]);
-      }
-
-      if (!boxes.has(currBoxIndex)) {
-        boxes.set(currBoxIndex, [board[i][j]]);
-      } else {
-        boxes.set(currBoxIndex, [...boxes.get(currBoxIndex), board[i][j]]);
-      }
+  for (let i = 0; i < nums.length - 1; i++) {
+    max = Math.max(max, nums[i] + i);
+    if (currMax === i) {
+      result++;
+      currMax = max;
     }
   }
-
-  return true;
+  return result;
 };
 
-console.log(
-  isValidSudoku([
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", "6", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-
-    [".", ".", ".", ".", "8", ".", ".", ".", "."],
-    ["9", ".", ".", ".", "7", "5", ".", ".", "."],
-    [".", ".", ".", ".", "5", ".", ".", "8", "."],
-
-    [".", ".", "9", ".", ".", ".", ".", ".", "."],
-    ["2", ".", "6", ".", ".", ".", ".", ".", "."],
-    [".", ".", ".", ".", ".", ".", ".", ".", "."],
-  ])
-);
+console.log(jump([2, 3, 0, 1, 4]));
