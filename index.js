3f1f7446600b757c8aa614d460961fc906d7a70d
Edwin <edwinshi1021@hotmail.com>
2024-01-04 20:06:13 -0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var jump = function (nums) {
-  let max = 0;
-  let currMax = 0;
-  let result = 0;
+var hIndex = function (citations) {
+  arr = citations.sort((a, b) => a - b);
+  let count = 0;
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    max = Math.max(max, nums[i] + i);
-    if (currMax === i) {
-      result++;
-      currMax = max;
+  for (let i = citations.length - 1; i >= 0; i--) {
+    if (citations[i] > count) {
+      count++;
     }
   }
-  return result;
+  return count;
 };
 
-console.log(jump([2, 3, 0, 1, 4]));
+console.log(hIndex([3, 0, 6, 1, 5]));
