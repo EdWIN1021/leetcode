0c4569a008bb592e53935e0a947db092b75a179c
Edwin <edwinshi1021@hotmail.com>
2024-01-11 16:12:10 -0800

    update

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-var merge = function (intervals) {
-  intervals = intervals.sort((a, b) => a[0] - b[0]);
-  const output = [intervals[0]];
-  for (let i = 1; i < intervals.length; i++) {
-    let currEnd = output[output.length - 1][1];
-    if (intervals[i][0] <= currEnd) {
-      output[output.length - 1][1] = Math.max(intervals[i][1], currEnd);
-    } else {
-      output.push(intervals[i]);
+var insert = function (intervals, newInterval) {
+  let [start, end] = newInterval;
+  let left = [];
+  let right = [];
+
+  for (const interval of intervals) {
+    const [first, last] = interval;
+
+    if (last < start) left.push(interval);
+    else if (first > end) right.push(interval);
+    else {
+      start = Math.min(start, first);
+      end = Math.max(end, last);
     }
   }
 
-  return output;
+  return [...left, [start, end], ...right];
 };
-
-merge([
-  [2, 3],
-  [2, 2],
-  [3, 3],
-  [1, 3],
-  [5, 7],
-  [2, 2],
-  [4, 6],
-]);
