e2d2a1b924350a8d1c2fad6abf817a90d47dfc01
Edwin <edwinshi1021@hotmail.com>
2024-01-15 15:41:44 -0800

    update

--- Solution.java
+++ Solution.java
@@ -1,28 +1,22 @@
-import java.util.Arrays;
+import java.util.Stack;
 
 class Solution {
     public static void main(String[] args){
-        int[][] points = {{10,16},{2,8},{1,6},{7,12}};
-        System.out.println(findMinArrowShots(points));
+        System.out.println(simplifyPath("/home//foo/"));
     }
 
-    public static int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
-      
-        int count = 1;
-        int[] start = points[0];
-
-        for(int i = 1; i < points.length; i++){
-            if(points[i][0] > start[1]){
-                count++;
-                start = points[i];
-            } else {
-                start[0] = Math.max(start[0], points[i][0]);
-                start[1] = Math.min(start[1], points[i][1]);
+    public static String simplifyPath(String path)  {
+        Stack<String> stack = new Stack<>();
+        for (String dir : path.split("/")) {
+            if(dir.equals("..") && !stack.isEmpty()){
+                stack.pop();
+            } else if (!dir.equals(".") && !dir.equals("..") && !dir.equals("")){
+                stack.push(dir);
             }
         }
-        return count;
+
+        String result = "";
+        for (String dir : stack) result = result + "/" + dir ;
+        return result.isEmpty() ? "/" : result;
     }
-
-
 } 
