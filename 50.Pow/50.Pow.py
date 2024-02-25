6a45f060663adb1389a7b9927d66e8b85e0439ad
Edwin Shi <edwinshi1021@hotmail.com>
2024-02-24 20:53:43 -0800

    update

--- 50.Pow/50.Pow.py
+++ 50.Pow/50.Pow.py
@@ -1,11 +1,16 @@
 def myPow(x, n):
-  result = 1
-  i = 0
+  res = helper(x, abs(n))
+  return res if n > 0 else 1 / res 
+
+def helper(x, n):
+  if x == 0:
+    return 0
   
-  while i < abs(n):
-    result *= x
-    i+=1
-
-  return result if n > 0 else 1 / result
-
+  if n == 0: 
+    return 1
+  
+  res = helper(x, n // 2)
+  res = res * res
+  return x * res if n % 2 else res
+  
 print(myPow(2.00000, -2))
